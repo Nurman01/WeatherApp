b2cfa3b94d0176afa34158c75b02b7262561b722
Nurman01 <150004216+Nurman01@users.noreply.github.com>
2024-01-30 14:21:22 +0600

    First Commit
    First Date: 30-01-2024

---
+++ utils/weatherData.js
@@ -0,0 +1,23 @@
+const request = require("request");
+
+const openWeatherMap = {
+  BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+  SECRET_KEY: "2d29cce56d199abc3e1253161519b978",
+};
+
+const weatherData = (address, callback) => {
+  const url =
+    openWeatherMap.BASE_URL +
+    encodeURIComponent(address) +
+    "&APPID=" +
+    openWeatherMap.SECRET_KEY;
+  console.log(url);
+  request({ url, json: true }, (error, data) => {
+    if (error) {
+      callback(true, "Unable to fetch data, Please try again. " + error);
+    }
+    callback(false, data?.body);
+  });
+};
+
+module.exports = weatherData;
